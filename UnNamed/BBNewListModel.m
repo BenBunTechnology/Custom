92062f1b450583e84d988da6046ca27bb0c6ce2b
BenBunTechnology <2771821779@qq.com>
2015-07-15 15:19:24 +0800

    添加修改

---
+++ UnNamed/BBNewListModel.m
@@ -0,0 +1,16 @@
+//
+//  BBNewListModel.m
+//  UnNamed
+//
+//  Created by T on 15/7/15.
+//  Copyright (c) 2015年 benbun. All rights reserved.
+//
+
+#import "BBNewListModel.h"
+
+@implementation BBNewListModel
++(NSArray *)modelPrepare:(id)obj{
+    
+    return obj[@"data"];
+}
+@end
